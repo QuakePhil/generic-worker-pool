2313666b3ad984203be9a3582ccd151167c1d37b
QuakePhil <phil.nyc@gmail.com>
2022-11-16 10:22:57 -0500

    include a basic test

---
+++ pool_test.go
@@ -0,0 +1,38 @@
+package pool
+
+import "testing"
+
+// TestPool counts how many bool Input() got.
+func TestPool(t *testing.T) {
+	worker := test{make(chan int, 1)}
+	New[bool](worker).Wait(1)
+	finalResult := <-worker.result
+	if finalResult != 3 {
+		t.Fatalf("Expected 3, got %d", finalResult)
+	}
+}
+
+type test struct {
+	result chan int
+}
+
+func (t test) Input(in chan bool) {
+	in <- true
+	in <- true
+	in <- true
+}
+
+func (t test) Process(i bool) bool {
+	return i
+}
+
+func (t test) Output(out chan bool) {
+	count := 0
+	for _ = range out {
+		count++
+	}
+	t.result <- count
+}
+
+func (t test) Done() {
+}
